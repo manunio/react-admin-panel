cb5bbca77c39ee4138a3b622b5d7e8019df53c33
manunio <max.manu.nair@gmail.com>
2022-06-12 22:50:58 +0530

    Added react charts and featured chart and added recharts pacakge

--- src/components/chart/Chart.tsx
+++ src/components/chart/Chart.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './chart.scss';
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
 function Chart(props: any) {
     return (
--- src/components/featured/Featured.tsx
+++ src/components/featured/Featured.tsx
@@ -1,10 +1,61 @@
 import React from 'react';
 import './featured.scss';
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import {CircularProgressbar} from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 
 function Featured(props: any) {
     return (
         <div className="featured">
-            Featured
+            <div className="top">
+                <div className="title">Total Revenue</div>
+                <MoreVertIcon fontSize="small"/>
+            </div>
+            <div className="bottom">
+                <div className="featuredChart">
+                    <CircularProgressbar value={70} text={"70%"} strokeWidth={5}/>
+                </div>
+                <p className="title">Total sales made today</p>
+                <p className="amount">$420</p>
+                <p className="desc">Previous transaction processing. Last payment may not be included.</p>
+                <div className="summary">
+                    <div className="item">
+                        <div className="itemTitle">
+                            Target
+                        </div>
+                        <div className="itemResult negative">
+                            <div className="resultAmount">
+                                <KeyboardArrowDownIcon fontSize="small"/>
+                                $12.4k
+                            </div>
+                        </div>
+                    </div>
+                    <div className="item">
+                        <div className="itemTitle">
+                            Last week
+                        </div>
+                        <div className="itemResult positive">
+                            <div className="resultAmount">
+                                <KeyboardArrowUpIcon fontSize="small"/>
+                                $12.4k
+                            </div>
+                        </div>
+                    </div>
+                    <div className="item">
+                        <div className="itemTitle">
+                            Last month
+                        </div>
+                        <div className="itemResult positive">
+                            <div className="resultAmount">
+                                <KeyboardArrowUpIcon fontSize="small"/>
+                                $12.4k
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
